12fea1c10d0aa8604294b0ba70fa67a4f2311f71
Julie Dursema <jmdursema@gmail.com>
2017-10-09 08:14:59 -0600

    commiting to new repo

--- form.js
+++ form.js
@@ -2,13 +2,28 @@ var guessButton = document.getElementById('guess');
 var inputField = document.getElementById('input');
 var lastGuess = document.getElementById('last-guess');
 var clearButton = document.getElementById('clear');
-var randomNumber=Math.floor(Math.random()*100+1);
 var highlow = document.querySelector('.highlow');
-var reset = document.getElementById('reset')
+var reset = document.getElementById('reset');
+var minimumInput = document.getElementById('minimum-input');
+var maximumInput = document.getElementById('maximum-input');
+var enterButton = document.getElementById('enter-min-max');
+var resetButton = document.getElementById('reset');
+
+guessButton.addEventListener('click', guessInput);
+enterButton.addEventListener('click', getRandomNumber);
+clearButton.addEventListener('click', clearField);
+guessButton.addEventListener('click', compare);
+guessButton.addEventListener('click', numberOutOfRange);
+inputField.addEventListener('keyup', disableGuess);
+inputField.addEventListener('keyup', disableClear);
+enterButton.addEventListener('click', disableMinMaxInputField);
+resetButton.addEventListener('click', reset);
+enterButton.addEventListener('click', disableReset);
+
 
 function guessInput (){
 	event.preventDefault();
-	console.log(inputField.value)
+	console.log(document.getElementById('input').value)
 	lastGuess.innerText=inputField.value;
 }
 
@@ -17,55 +32,61 @@ function clearField (){
 	inputField.value="";
 }
 
+function disableMinMaxInputField (){
+	minimumInput.disabled=true;
+	maximumInput.disabled=true;
+	enterButton.disabled=true;
+}
 
-console.log(randomNumber)
+function getRange (){
+	event.preventDefault();
+	console.log(minimumInput.value);
+	console.log(maximumInput.value);
+	minimumInput.innerText=minimumInput.value;
+	maximumInput.innerText=maximumInput.value;
+}
 
-
-// function createNumber (){
-// 	event.preventDefault();
-// 	console.log((Math.floor(Math.random()*100+1)))
-// }
-
-
+function getRandomNumber(){
+	randomNum = (Math.floor(Math.random()*(parseInt(maximumInput.value)-parseInt(minimumInput.value)+1)+(parseInt(minimumInput.value))))
+	console.log(randomNum);
+}
 
 function compare (){
-	if (inputField.value == randomNumber){
-		console.log("BOOM!");
-		highlow.innerText= "BOOM!"
+	var makeThingsNumber = parseInt(inputField.value);
+
+
+	if (makeThingsNumber === randomNum){
+		levelUp();
 	}
-	else if (inputField.value < randomNumber){
-		console.log("Your guess is too low");
+	else if (makeThingsNumber < randomNum){
 		highlow.innerText = "Your guess is too low";
 	}
-	else if (inputField.value > randomNumber){
-		console.log("Your guess is too high");
+	else if (makeThingsNumber > randomNum){
 		highlow.innerText = "Your guess is too high";
 	}
 	else {
-		console.log("Invalid");
 		highlow.innerText = "Invalid"
 	}
 }
 
 function numberOutOfRange() {
-	if(inputField.value >= 101){
+	if(inputField.value >= (parseInt(maximumInput.value)+1)){
 		highlow.innerText= 'That number is out of range';
 		clearField();
 	}
-	else if(inputField.value <= 0){
+	else if(inputField.value <= (parseInt(minimumInput.value)-1)){
 		highlow.innerText = "That number is out of range";
 		clearField();
 	}
 	else if (isNaN(inputField.value)){
 		highlow.innerText= "That is not a number";
 	}
-
 }
 
 function disableGuess(){
 	if(inputField.value >= 101) {guessButton.disabled= true;
 	}
-	else if(inputField.value <= 0){guessButton.disabled= true;
+	else if(inputField.value <= -101){guessButton.disabled= true;
 	}
 	else if (isNaN(inputField.value)){guessButton.disabled= true;
 	}
@@ -73,21 +94,24 @@ function disableGuess(){
 }
 
 function disableClear(){
-	if(inputField.value == 3) {clearButton.disabled=false;
+	if(inputField.value == '') {clearButton.disabled = true;
 	}
-	else {inputField.disabled=true;
+	else {clearButton.disabled = false;
 	}
 }
 
+function reset(){
+	document.getElementById('main').reset();
+}
 
+function disableReset(){
+	resetButton.disabled=false;
+}
 
-guessButton.addEventListener('click', guessInput)
-guessButton.addEventListener('click', randomNumber)
-clearButton.addEventListener('click', clearField)
-guessButton.addEventListener('click', compare)
-guessButton.addEventListener('click', numberOutOfRange)
-inputField.addEventListener('keyup', disableGuess)
-inputField.addEventListener('keyup', disableClear)
-
-
-
+function levelUp(){
+	if(highlow.innerText="BOOM! Level Up!"){
+		maximumInput.value = (parseInt(maximumInput.value)+10);
+		minimumInput.value = (parseInt(minimumInput.value)-10);
+		getRandomNumber()
+	}
+}
